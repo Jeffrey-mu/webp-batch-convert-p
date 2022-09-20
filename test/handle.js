2410c72a11430903834dcc3df4c56a8365719c69
Jeffrey <1115378579@qq.com>
2022-09-20 16:11:35 +0800

    feat: 添加覆盖模式，如图片已被转换则跳过，默认跳过

--- test/handle.js
+++ test/handle.js
@@ -1,10 +1,12 @@
 const convert = require('../lib/cwebp');
-// const convert = require('webp-batch-convert');
+// const  = require('webp-batch-convert');
 console.log(convert)
 let res;
 let startTime = +new Date();
 console.log(new Date())
 // 示例一: 生成 img 目录下的图片文件至 webp 目录
-res = convert('./image', './web', { replace_suffix: true, gif: true });
+res = convert('./image', './web', { replace_suffix: true, gif: true, cover: true });
 console.log((+new Date() - startTime) / 1000)
 console.log(res);
+
+
